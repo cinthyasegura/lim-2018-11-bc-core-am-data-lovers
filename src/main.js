79373d3b2ef0efc4279d3a67776f551730ef7d06
cinthyasegura <cinthyaless@gmail.com>
2018-12-13 18:21:07 -0500

    mostrando lista de paises en el html

--- src/main.js
+++ src/main.js
@@ -1,23 +1,41 @@
-const indicatorsName = WORLDBANK.PER.indicators.map(indicator => indicator.indicatorName);
-  document.getElementById("demo").innerHTML = indicatorsName.join()
+// const indicatorsName = WORLDBANK.PER.indicators.map(indicator => indicator.indicatorName);
+//   document.getElementById("demo").innerHTML = indicatorsName.join()
 
-
-const result = Object.keys(WORLDBANK.PER);
-console.log(result)
+// copy of WORLDBANK object 
+// const worldbankCopy = Object.entries(WORLDBANK);
+// const copy = (arr) => {
+//   const newArr = [];
+//   for(let i = 0; i < arr.length; i++){
+//     newArr.push(Object.assign({}, arr[i]))
+//   } 
+//   return newArr;
+//  }
+// console.log(copy(worldbankCopy))
 
 // const result = Object.values(WORLDBANK.PER);
 // console.log(result)
 
-const resultado = Object.keys(WORLDBANK.PER.indicators.data)
-console.log(resultado)
+//listado de paises
+const arrCountries = Object.keys(WORLDBANK);
+let countriesList = '';
+arrCountries.forEach(countries => {
+  const list = `<li> ${countries} </li>`;
+  countriesList += list;
+});
+
+const ulCountriesList = document.getElementById('countries-list');
+ulCountriesList.innerHTML = countriesList;
+
+const keysOfPer = Object.keys(WORLDBANK.PER);
+console.log(keysOfPer)
+
+
+// const resultado = Object.keys(WORLDBANK.PER.indicators.data)
+//console.log(resultado)
 
 
 
-// var arrProp = Object.keys(WORLDBANK)
-// for(var i = 0; i < arrProp.length; i++){
-//     WORLDBANK[arrProp[i]]
-// }
-//console.log(WORLDBANK)
+// 
 
 // var result = Object.keys(WORLDBANK.PER.indicators.data).map(key => {
 //     return Number(key), WORLDBANK.PER.indicators.data[key];
