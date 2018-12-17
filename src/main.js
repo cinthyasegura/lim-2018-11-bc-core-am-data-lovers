99ceac7caaf4abbe759de4db687ca94d2c937474
brendapsd <brenda.sd.15@gmail.com>
2018-12-17 11:04:50 -0500

    mostrando en data en una tabla en consola

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
+console.table(WORLDBANK.PER.indicators);
+
 //const result = Object.entries(WORLDBANK.PER.indicators[0].data);
 
 //console.log(result[0]);
 
-const population = WORLDBANK.PER.indicators.filter(pop => {
+/*const population = WORLDBANK.PER.indicators.filter(pop => {
   return pop.indicatorCode.slice(0,6) === 'SP.POP';
 });
 
 console.log(population);
 document.getElementById('demo').innerHTML = population;
-
+*/
 
 // console.log(Object.keys(WORLDBANK));
 /*
