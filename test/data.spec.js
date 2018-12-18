41ea2a908fd0a032db0d3f8f9d797516c09904d0
cinthyasegura <cinthyaless@gmail.com>
2018-12-18 15:58:06 -0500

    pruebas de test typeof

--- test/data.spec.js
+++ test/data.spec.js
@@ -10,3 +10,14 @@ require('../src/data.js');
 //     expect(example()).toBe('example');
 //   });
 // });
+
+describe('worldbank', () => {
+  it('deberia ser un objeto', () => {
+    // expect(typeof worldbank).toBe('object');
+  });
+  describe('worldbank.populationFilter', () => {
+    it('deberia ser una funcion', () => {
+      expect(typeof worldbank.populationFilter).toBe('function');
+    });
+  });
+});
