57c65e1b97e22c0db9eb0931d09414b639876611
cinthyasegura <cinthyaless@gmail.com>
2018-12-17 21:25:04 -0500

    restructuracion del html y separacion del dom y logica para filtrado por tipo de indicador

--- src/main.js
+++ src/main.js
@@ -1,88 +1,124 @@
-//const result = Object.entries(WORLDBANK.PER.indicators[0].data);
 
-//console.log(result[0]);
 
-const mexicoMap = document.getElementById('mexico-map');
-const peruMap = document.getElementById('peru-map');
+const populationIndicators = document.getElementById('population-indicators');
+const laboralIndicators = document.getElementById('laboral-indicators');
+const unemploymentIndicators = document.getElementById('unemployment-indicators');
+const educationIndicators = document.getElementById('education-indicators');
+const violenceIndicators = document.getElementById('violence-indicators');
+const map = document.getElementsByClassName('map');
 
 
 let populationList = '';
 const population = WORLDBANK.PER.indicators
-  .filter(pop => pop.indicatorCode.slice(0, 6) === 'SP.POP')
-  .map(pop => pop.indicatorName)
-  .forEach(pop => {
-    const popList = `<li><a href=#> ${pop} </a></li>`;
+worldbank.populationFilter(population, 'SP.POP').forEach(pop => {
+  const popList = `<div><a href=#> ${pop.indicatorName} </a></div>`;
     populationList += popList;
-  });
-document.getElementById('population-list').innerHTML = populationList;
-//document.getElementById('demo').innerHTML = population;
-
-let educationList = '';
-const education = WORLDBANK.PER.indicators
-  .filter(edu => edu.indicatorCode.slice(0, 2) === 'SE')
-  .map(edu => edu.indicatorName)
-  .forEach(edu => {
-    const eduList = `<li><a href=#> ${edu} </a></li>`;
-    educationList += eduList;
-  });
-document.getElementById('education-list').innerHTML = educationList;
+});
+populationIndicators.innerHTML = populationList;
 
 let laboralList = '';
 const laboral = WORLDBANK.PER.indicators
-  .filter(lab => lab.indicatorCode.slice(0, 6) === 'SL.TLF')
-  .map(lab => lab.indicatorName)
-  .forEach(lab => {
-    const labList = `<li><a href=#> ${lab} <a/></li>`;
+worldbank.laboralFilter(laboral, 'SL.TLF').forEach(lab => {
+  const labList = `<div><a href=#> ${lab.indicatorName} <a/></div>`;
     laboralList += labList;
-  });
-document.getElementById('laboral-list').innerHTML = laboralList;
+});
+laboralIndicators.innerHTML = laboralList;
 
 let unemploymentList = '';
 const unemployment = WORLDBANK.PER.indicators
-  .filter(unemployed => unemployed.indicatorCode.slice(0, 6) === 'SL.UEM')
-  .map(unemployed => unemployed.indicatorName)
-  .forEach(unemployed => {
-    const unemployedList = `<li><a href=#> ${unemployed} </a></li>`;
+worldbank.unemploymentFilter(unemployment, 'SL.UEM').forEach(unemployed => {
+  const unemployedList = `<div><a href=#> ${unemployed.indicatorName} </a></div>`;
     unemploymentList += unemployedList;
-  });
-document.getElementById('unemployment-list').innerHTML = unemploymentList;
+});
+unemploymentIndicators.innerHTML = unemploymentList;
+
+let educationList = '';
+const education = WORLDBANK.PER.indicators
+worldbank.educationFilter(education, 'SE').forEach(edu => {
+  const eduList = `<div><a href=#> ${edu.indicatorName} </a></div>`;
+    educationList += eduList;
+});   
+educationIndicators.innerHTML = educationList;
 
 let violenceList = '';
 const violence = WORLDBANK.PER.indicators
-  .filter(violent => violent.indicatorCode.slice(0, 6) === 'SG.VAW')
-  .map(violent => violent.indicatorName)
-  .forEach(violent => {
-    const violentList = `<li><a href=#> ${violent} </a></li>`;
+worldbank.violenceFilter(violence, 'SG.VAW').forEach(violent => {
+  const violentList = `<div><a href=#> ${violent.indicatorName} </a></div>`;
     violenceList += violentList;
-  });
-document.getElementById('violence-list').innerHTML = violenceList;
-
-peruMap.addEventListener('click', () =>{
-    document.getElementById('welcome-page').style.display = 'none';
-    document.getElementById('lists').style.display = 'block'; 
 });
+violenceIndicators.innerHTML = violenceList;
+
+document.getElementById('btn-map').addEventListener('click', () =>{
+  document.getElementById('welcome-page').style.display = 'none';
+  document.getElementById('first-page').style.display = 'block'; 
+  document.getElementById('indicators-icons').style.visibility = 'hidden';
+});
+
+document.getElementById('peru-map').addEventListener('click', () =>{
+    // document.getElementById('first-page').style.display = 'none';
+    // document.getElementById('second-page').style.display = 'block';
+  document.getElementById('indicators-icons').style.visibility = 'visible'; 
+});
+document.getElementById('population-option').addEventListener('click', () => {
+  map[0].style.display = 'none';
+  populationIndicators.style.display = 'block';
+  laboralIndicators.style.display = 'none';
+  unemploymentIndicators.style.display = 'none';
+  educationIndicators.style.display = 'none';
+  violenceIndicators.style.display = 'none';
+});
+document.getElementById('laboral-option').addEventListener('click', () => {
+  populationIndicators.style.display = 'none';
+  laboralIndicators.style.display = 'block';
+  unemploymentIndicators.style.display = 'none';
+  educationIndicators.style.display = 'none';
+  violenceIndicators.style.display = 'none';
+});
+document.getElementById('unemployment-option').addEventListener('click', () => {
+  populationIndicators.style.display = 'none';
+  laboralIndicators.style.display = 'none';
+  unemploymentIndicators.style.display = 'block';
+  educationIndicators.style.display = 'none';
+  violenceIndicators.style.display = 'none';
+});
+document.getElementById('education-option').addEventListener('click', () => {
+  populationIndicators.style.display = 'none';
+  laboralIndicators.style.display = 'none';
+  unemploymentIndicators.style.display = 'none';
+  educationIndicators.style.display = 'block';
+  violenceIndicators.style.display = 'none';
+});
+document.getElementById('violence-option').addEventListener('click', () => {
+  populationIndicators.style.display = 'none';
+  laboralIndicators.style.display = 'none';
+  unemploymentIndicators.style.display = 'none';
+  educationIndicators.style.display = 'none';
+  violenceIndicators.style.display = 'block';
+});
+
+
 //select option
 // function showIndicators() {
 //   const selectIndicator = document.getElementById('select-indicator');
 //   const userInput = selectIndicator.options[selectIndicator.selectedIndex].value;
 //   if(userInput == 'population'){
-//     document.getElementById('population-list').style.visibility = 'visible';
+//     document.getElementById('population-list').style.display = 'block';
 //   }
 //   else if(userInput == 'labor'){
-//     document.getElementById('laboral-list').style.visibility = 'visible';
-//     document.getElementById('population-list').style.visibility = 'hidden';
+//     document.getElementById('laboral-list').style.display = 'block';
+//     document.getElementById('population-list').style.display = 'none';
 //   }
 //   else if(userInput == 'education'){
-//     document.getElementById('education-list').style.visibility = 'visible';
-//     document.getElementById('laboral-list').style.visibility = 'hidden';
+//     document.getElementById('education-list').style.display = 'block';
+//     document.getElementById('laboral-list').style.display = 'none';
 //   }
 //   else if(userInput == 'unemployment'){
-//     document.getElementById('unemployment-list').style.visibility = 'visible';
-//     document.getElementById('education-list').style.visibility = 'hidden';
+//     document.getElementById('unemployment-list').style.display = 'block';
+//     document.getElementById('education-list').style.display = 'none';
 //   }
 //   else if(userInput == 'violence'){
-//     document.getElementById('violence-list').style.visibility = 'visible';
-//     document.getElementById('unemployment-list').style.visibility = 'hidden';
+//     document.getElementById('violence-list').style.display = 'block';
+//     document.getElementById('unemployment-list').style.display = 'none';
 //   }
 // }
 // selectIndicator.addEventListener('change', showIndicators => {
@@ -101,3 +137,7 @@ for(let i=0; i<arrWORLBANK.length; i++){
 
 console.log(newArrIndicador);
 */
+
+
+const datos = Object.entries(WORLDBANK.PER.indicators[0].data)
+console.log(datos)
