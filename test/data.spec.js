d333304c557e52a6e44dbbae8c4079380779633a
brendapsd <brenda.sd.15@gmail.com>
2018-12-19 19:57:09 -0500

    se realizo test co typeof y se modifico la barra de navegación

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,23 @@
 require('../src/data.js');
 
 
- describe('worldbank', () => {
-   it('Deberia ser un objeto', () => {
-     expect(typeof worldbank).toBe('object');
-   });
+// describe('example', () => {
+//   it('is a function', () => {
+//     expect(typeof example).toBe('function');
+//   });
 
 //   it('returns `example`', () => {
-//   expect(example()).toBe('example');
+//     expect(example()).toBe('example');
 //   });
 // });
+
+describe('worldbank', () => {
+  it('deberia ser un objeto', () => {
+    expect(typeof worldbank).toBe('object');
+  });
+  describe('worldbank.populationFilter', () => {
+    it('deberia ser una funcion', () => {
+      expect(typeof worldbank.populationFilter).toBe('function');
+    });
+  });
+});
